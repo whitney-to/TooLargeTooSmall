16243fc64775fbb5309613088bc4d0427ddef73f
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-01 15:07:41 -0500

    move guessing to a separate method and keep track of guesses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,73 +5,59 @@ import java.util.Scanner;
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-    public static void main(String[] args){
-        boolean isPlaying = true;
-        boolean isInGame = true;
+    public static void GuessingANumber() {
+        Scanner scanner = null;
+        int userInput = -1;
+        int previousGuess = 0;
+        int numberGuesses = 0;
 
-        while(isPlaying) {
-            if(isInGame){
-                Scanner scanner = null;
-                int userInput = -1;
+        // create a random integer for user to guess
+        Random randomInteger = new Random();
+        int expectedNumber = randomInteger.nextInt(101);
+        System.out.println(expectedNumber);
+        do {
+            // asking for user input
+            System.out.println("Guess number from 1 - 100 or enter (0) to exit: ");
+            scanner = new Scanner(System.in);
 
-                // create a random integer for user to guess
-                Random randomInteger = new Random();
-                int expectedNumber  = randomInteger.nextInt(101);
-                System.out.println(expectedNumber);
-
-                do{
-                    // asking for user input
-                    System.out.println("Guess number from 1 - 100 or enter (0) to exit: ");
-                    scanner = new Scanner(System.in);
-                    userInput = scanner.nextInt();
-
-                    // check user input
-                    if(userInput == 0){
-                        System.out.println("You have exited the game!");
-                        isPlaying = false;
-                        isInGame = false;
-                        return;
-                    }
-                    //if user input larger than expected number
-                    else if(userInput > expectedNumber){
-                        System.out.println("Too large!");
-                        continue;
-                    }
-                    //if user input smaller than expected number
-                    else if(userInput < expectedNumber) {
-                        System.out.println("Too small!");
-                        continue;
-                    }
-                    //if user input is equal to the expected number
-                    else if(userInput == expectedNumber) {
-                        System.out.println("Congratulation! You have guessed the number.");
-                        isInGame = false;
-                        break;
-                    } else {
-                        continue;
-                    }
-                } while(userInput != expectedNumber);
-
-            }
-            System.out.println("\"Do you want to continue Playing? (Y/N): \"");
-            Scanner scanner2 = new Scanner(System.in);
-            String userInput2 = scanner2.nextLine();
-            if(userInput2.equalsIgnoreCase("Y")){
-                isInGame = true;
-                continue;
-            } else if (userInput2.equalsIgnoreCase("N")){
-                isPlaying  = false;
-                isInGame = false;
-                break;
-            } else {
-                isInGame = false;
+            // check if input is not integer, or if input > 100 or < 0
+            if (!scanner.hasNextInt()) {
                 System.out.println("Invalid input, Please try again!");
+                continue;
+            } else { // if input is integer and within range 1-100
+
+                // get user input
+                userInput = scanner.nextInt();
+
+                // if user input is out of range
+                if (userInput > 100 || userInput < 0) {
+                    System.out.println("Input out of range!");
+                    continue;
+                } else if (userInput == 0) {
+                    System.out.println("You have exited the game!");
+                    return;
+                } else if (userInput > expectedNumber) {
+                    System.out.println("Too large!");
+                    if(userInput!=previousGuess){
+                        previousGuess = userInput;
+                        numberGuesses++;
+                    }
+                } else if (userInput < expectedNumber) {
+                    System.out.println("Too small!");
+                    if(userInput!=previousGuess){
+                        previousGuess = userInput;
+                        numberGuesses++;
+                    }
+                } else {
+                    numberGuesses++;
+                    System.out.println(numberGuesses+" guesses");
+                    System.out.println("correct guess!");
+                }
             }
-        }
+        } while (userInput != expectedNumber) ;
+    }
 
-
-
-
-
+    public static void main(String[] args){
+                GuessingANumber();
     }
 }
