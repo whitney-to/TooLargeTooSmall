8fd6f267e4010d2ee541c13ba98a49ca7aba74a5
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-01 16:20:36 -0500

    close scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -55,6 +55,7 @@ public class Main {
                 }
             }
         } while (userInput != expectedNumber) ;
+        scanner.close();
     }
 
     public static void main(String[] args){
