bb6a44d1ab9b8fa397e46e7adf7723067707cc2c
Nguyet <nguyet@zipcodersmbp103.mynetworksettings.com>
2023-02-28 21:07:09 -0500

    create and initialize Random object to get random integer from 0-100

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Random randomInteger = new Random();
+        System.out.println(randomInteger.nextInt(100));
     }
 }
